49bd54e0110b8e51f2fcad8c1e21d532deb50f69
de la O <julio.espinoza@itbmexico.com>
2025-10-02 15:02:51 -0600

    Crear rutas y manejar peticiones

--- Echo/main.go
+++ Echo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"Echo/routes"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"net/http"
 )
 
 func main() {
@@ -12,21 +12,9 @@ func main() {
 
 	//e.Use(middleware.Logger())
 
+	routes.SetupRoutes(e)
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "method=${method}, uri=${uri}, status=${status}, time=${latency_human}\n",
 	}))
-
-	e.GET("/", func(c echo.Context) error {
-		//return c.String(http.StatusOK, "Hello, World!")
-		return c.JSON(http.StatusOK, map[string]string{
-			"message": "Hello From Echo",
-		})
-	})
-
-	e.GET("/saludo", func(c echo.Context) error {
-		return c.String(http.StatusOK, "saludos desde Echo")
-
-	})
-
 	e.Start(":8080")
 }
--- Echo/routes/routes.go
+++ Echo/routes/routes.go
@@ -1,1 +1,28 @@
 package routes
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func SetupRoutes(e *echo.Echo) {
+
+	e.GET("/", func(c echo.Context) error {
+		//return c.String(http.StatusOK, "Hello, World!")
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "Hello From Echo",
+		})
+	})
+
+	e.GET("/saludo", func(c echo.Context) error {
+		return c.String(http.StatusOK, "saludos desde Echo")
+
+	})
+
+	e.GET("/saludo/:nombre", func(c echo.Context) error {
+		nombre := c.Param("nombre")
+		return c.String(http.StatusOK, "¡Hola,"+nombre+"!")
+
+	})
+
+}
